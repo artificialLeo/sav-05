3c09b3f979dbe252d4a3d04f6856f70f09f2f653
khorkovenko <khorkovenko>
2022-10-08 20:17:23 +0300

    fp

--- sav/src/main/java/com/example/sav/SavApplication.java
+++ sav/src/main/java/com/example/sav/SavApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SavApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SavApplication.class, args);
 	}
